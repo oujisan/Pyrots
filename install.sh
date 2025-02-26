4037d859846aa81875c6e1cf986dc7d31ced9aca
Muhammad Syaifur Rozi <141596017+oujisan@users.noreply.github.com>
2025-02-26 21:07:52 +0700

    fix tittle

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 chmod +x ./pyrots.py
 sudo cp pyrots.py /usr/local/bin/pyrots
 sudo sed -i 's/\r$//' "/usr//local/bin/pyrots"
-echo "PyROT has been successfully installed! You can run it using: pyrots"
+echo "PyROTS has been successfully installed! You can run it using: pyrots"
